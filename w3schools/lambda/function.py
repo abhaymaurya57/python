5e4766a5426f58d626f07d78ef23f39158455701
Your Name <abh.mrya@gmail.com>
2025-01-29 12:06:31 +0530

    edit

--- w3schools/lambda/function.py
+++ w3schools/lambda/function.py
@@ -1,4 +1,13 @@
-def myfunc(n):
-    return lambda a: a*n
-x =myfunc(15)
+def myfunc(n,m):
+    return lambda a: a*n-m
+x =myfunc(15,2)
 print(x(5))
+
+def myfunc(n):
+  return lambda a : a * n
+
+mydoubler = myfunc(2)
+mytripler = myfunc(3)
+
+print(mydoubler(11))
+print(mytripler(11))
