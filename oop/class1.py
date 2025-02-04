394adb9628950ab905b5030c6aa4b4ac4a6cb688
Your Name <abhaymaurya54321@gmail.com>
2025-02-04 19:53:05 +0530

    class

---
+++ oop/class1.py
@@ -0,0 +1,32 @@
+# class student:
+#     def __init__(self,cls,name):
+#         self.i=cls
+#         self.r =name
+
+# data = student(5,'Abhay')
+# print(data.i)
+# print(data.r)
+##########################
+# class table:
+    # counter = 1
+    # while counter<10:
+    # def __init__(self,number):
+    #     self.i=number
+    
+
+        
+class table:
+
+    # def __init__(self):
+    #     self.counter  = 1
+    
+    def print_table(self):
+        self.counter  = 1
+        while self.counter < 10:
+  
+            self.counter += 1
+            print(self.counter*2)
+        # print(self.counter*2)
+
+obj = table()
+obj.print_table()
