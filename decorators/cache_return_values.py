59bf5c5fda95d9d1672ab72a86887edbfee8452a
abhaymaurya57 <abhaymaurya54321@gmail.com>
2025-08-24 23:49:37 +0530

    decorater

--- decorators/cache_return_values.py
+++ decorators/cache_return_values.py
@@ -4,6 +4,7 @@ def cache(func):
     cache_value = {}
     print(cache_value)
     def wrapper(*args):
+        print(cache_value)
         if args in cache_value:
             return cache_value[args]
         result = func(*args)
@@ -13,12 +14,22 @@ def cache(func):
 
 @cache
 def long_running_function(a,b):
-    time.sleep(4)
+    time.sleep(2)
     return a+b
 
 print(long_running_function(2,3))
-print(long_running_function(2,3))
+print(long_running_function(20000,34343))
 print(long_running_function(4,3))
 print(long_running_function(4,3))
 print(long_running_function(4,5))
 print(long_running_function(5,3))
+print(long_running_function(20000,34343))
+print(long_running_function(20000,34343))
+print(long_running_function(20000,34343))
+print(long_running_function(20000,34343))
+print(long_running_function(4,3))
+print(long_running_function(4,3))
+print(long_running_function(4,3))
+print(long_running_function(4,3))
+print(long_running_function(4,3))
+print(long_running_function(4,3))
---
+++ decorators/decorator.py
@@ -0,0 +1,26 @@
+''' A Decorator in Python is a function that receives another function as argument.
+The argument function is the one to be decorated by decorator.
+The behaviour of argument function is extended by the decorator without actually modifying it. '''
+import timeit
+# Example 1:
+def my_function(x):
+   print("The number is=",x)
+def my_decorator(some_function,num):
+   def wrapper(num):
+      print("Inside wrapper to check odd/even")
+      if num%2 == 0:
+         ret= "Even"
+      else:
+         ret= "Odd!"
+      some_function(num)
+      return ret
+   print ("wrapper function is called")
+   return wrapper
+no=10
+my_function = my_decorator(my_function, no)
+print ("It is ",my_function(no))
+
+
+# timeit with lambda
+execution_time = timeit.timeit(lambda: my_function, number=1)
+print("Execution time:", execution_time)
