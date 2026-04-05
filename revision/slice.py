20fefcbe08a422801df97b09be721a2b853628ac
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-04-05 23:50:51 +0530

    slice

---
+++ revision/slice.py
@@ -0,0 +1,13 @@
+strr = "Abhay Maurya"
+
+print(strr[:])
+print(strr[0:1])
+print(strr[5:10])
+print(strr[-1:2])
+print(strr[-4:-1])
+print(strr[-9::2])
+print(strr[::2])
+print(strr[1:6:2])
+# print(strr[:])
+# print(strr[:])
+# print(strr[:])
