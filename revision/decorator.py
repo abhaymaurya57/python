f041ad008a246278994a0d5809c06a85ac74be1d
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-04-07 23:45:16 +0530

    decorator

---
+++ revision/decorator.py
@@ -0,0 +1,16 @@
+
+def deco(func):
+    def wrapper(*args, **kwargs):
+        print("before the table.")
+        func(*args, **kwargs)
+        print("after the table.")
+    return wrapper
+
+
+@deco
+def table_print(n):
+    for i in range(1,11):
+        print(f'{n} X {n} = {n*i}')
+        # print(i)
+
+table_print(10)
