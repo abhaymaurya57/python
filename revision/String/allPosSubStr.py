37aff20fd652c89efb9b175fa3eab2a6771be2fb
abhaymaurya57 <abhaymaurya54321@gmail.com>
2026-07-05 15:31:25 +0530

    AllPosSubStr

---
+++ revision/String/allPosSubStr.py
@@ -0,0 +1,17 @@
+# s=input("Enter main string:")
+# subs=input("Enter sub string")
+s="aabfdabfdsfbafdfaa"
+subs="a"
+Flag=False
+pos=-1
+
+n = len(s)
+
+while True:
+    pos=s.find(subs,pos+1,n)
+    if pos==-1:
+        break
+    print("found at position",pos)
+    Flag=True
+if Flag==False:
+    print("Not Found")
